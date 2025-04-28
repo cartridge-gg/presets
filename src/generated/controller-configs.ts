b3806584d11a4f06116785d87ee9baca6b578649
Nils <117471699+NilsDelage@users.noreply.github.com>
2025-04-28 14:01:20 +0200

    feat: metacube preset (#67)

--- src/generated/controller-configs.ts
+++ src/generated/controller-configs.ts
@@ -2172,6 +2172,50 @@ export const configs: ControllerConfigs = {
       },
     },
   },
+  metacube: {
+    origin: ["*.metacube.games", "metacube.games"],
+    theme: {
+      colors: {
+        primary: "#11ffbb",
+      },
+      cover: "https://static.cartridge.gg/presets/metacube/cover.png",
+      icon: "https://static.cartridge.gg/presets/metacube/icon.png",
+      name: "Metacube",
+      optimizedCover: {
+        webp: {
+          "768": "cover@768.webp",
+          "1024": "cover@1024.webp",
+          "1440": "cover@1440.webp",
+        },
+        jpg: {
+          "768": "cover@768.jpg",
+          "1024": "cover@1024.jpg",
+          "1440": "cover@1440.jpg",
+        },
+        png: {
+          "768": "cover@768.png",
+          "1024": "cover@1024.png",
+          "1440": "cover@1440.png",
+        },
+      },
+      optimizedIcon: {
+        webp: {
+          "16": "icon@16.webp",
+          "32": "icon@32.webp",
+          "64": "icon@64.webp",
+          "128": "icon@128.webp",
+          "256": "icon@256.webp",
+        },
+        png: {
+          "16": "icon@16.png",
+          "32": "icon@32.png",
+          "64": "icon@64.png",
+          "128": "icon@128.png",
+          "256": "icon@256.png",
+        },
+      },
+    },
+  },
   "metal-slug": {
     origin: "https://metalslug.starkarcade.com/",
     chains: {
@@ -3400,6 +3444,7 @@ export const availableConfigs = [
   "force-prime",
   "jokers-of-neon",
   "loot-survivor",
+  "metacube",
   "metal-slug",
   "nums",
   "paved",
