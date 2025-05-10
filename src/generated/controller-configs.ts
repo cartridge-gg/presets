9d898427e3fe8cf0abcc450a0499bf84c5c311e6
Yun <broody@gmail.com>
2025-05-09 17:53:59 -1000

    Add Chaos Surfer (#75)

--- src/generated/controller-configs.ts
+++ src/generated/controller-configs.ts
@@ -691,6 +691,50 @@ export const configs: ControllerConfigs = {
       },
     },
   },
+  "chaos-surfer": {
+    origin: "place-holder",
+    theme: {
+      colors: {
+        primary: "#E66666",
+      },
+      cover: "https://static.cartridge.gg/presets/chaos-surfer/cover.png",
+      icon: "https://static.cartridge.gg/presets/chaos-surfer/icon.png",
+      name: "Chaos Surfer",
+      optimizedCover: {
+        webp: {
+          "768": "cover@768.webp",
+          "1024": "cover@1024.webp",
+          "1440": "cover@1440.webp",
+        },
+        jpg: {
+          "768": "cover@768.jpg",
+          "1024": "cover@1024.jpg",
+          "1440": "cover@1440.jpg",
+        },
+        png: {
+          "768": "cover@768.png",
+          "1024": "cover@1024.png",
+          "1440": "cover@1440.png",
+        },
+      },
+      optimizedIcon: {
+        webp: {
+          "16": "icon@16.webp",
+          "32": "icon@32.webp",
+          "64": "icon@64.webp",
+          "128": "icon@128.webp",
+          "256": "icon@256.webp",
+        },
+        png: {
+          "16": "icon@16.png",
+          "32": "icon@32.png",
+          "64": "icon@64.png",
+          "128": "icon@128.png",
+          "256": "icon@256.png",
+        },
+      },
+    },
+  },
   coloniz: {
     origin: ["*.coloniz.xyz"],
     chains: {
@@ -3726,6 +3770,7 @@ export const availableConfigs = [
   "budokan",
   "bytebeasts-tamagotchi",
   "cartridge",
+  "chaos-surfer",
   "coloniz",
   "credit",
   "dark-shuffle",
